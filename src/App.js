77c3f6e197c050647d52865125ab23e8fc9b92eb
Christina Li <christinayxli@gmail.com>
2022-12-28 13:45:25 -0500

    added chat bot feature on homepage.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Homepage from './components/homepage';
 import Navbar from './components/navbar'
+import KommunicateChat from './components/chat';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Homepage/>
+      <KommunicateChat/>
     </div>
   );
 }
