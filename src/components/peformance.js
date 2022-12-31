651dec860d247a6991a8363be7de979b00be869d
Christina Li <christinayxli@gmail.com>
2022-12-30 21:54:12 -0500

    finished performance page

--- src/components/peformance.js
+++ src/components/peformance.js
@@ -3,23 +3,62 @@ import Navbar from './navbar'
 import { Rating } from '@mui/material';
 import './performance.css';
 import HoverRating from './stars'
+import RadioGroupRating from './viberating'
 function Performance() {
   
   return (
     <div>
         <Navbar buttontxt = 'Back to Home' current = 'Peformance' className = 'bg-[#f4e9e3] px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0'></Navbar>
         <div className = 'spacing'></div>
-        <h1 className = 'tracking-wide text-center font-sans font-medium text-4xl'> 🌟 Tracking Student <mark className = 'text-[#473946] bg-[#e8dce2]'> &nbsp; Performance &nbsp;</mark> 🌟</h1>
-        <h2 className = 'tracking-wide text-xl mt-10 ml-20'> Enter Student Name:</h2>
-        <form className=' w-1/3 mt-2 flex '>
-        <input type="text" className=' w-12 bg-[#eff2f7] ml-20 shadow-inner rounded-l p-2 flex-1' id='name'  placeholder='Enter Your Student`s Name here' />
+        <h1 className = 'tracking-wide text-center font-sans font-medium text-4xl'> 🌟 Tracking Student <mark className = 'text-black bg-[#e1ebed]'> &nbsp; Performance &nbsp;</mark> 🌟</h1>
+
+
+ <div class="grid gap-2 grid-cols-2 grid-rows-3">
+ <div>  
+       <h2 className = 'tracking-wide text-xl mt-10 ml-20'> Enter Student Name:</h2>
+        <form className=' w-1/2 mt-2 flex '>
+        <input type="text" className=' w-12 bg-[#eff2f7] ml-20 shadow-inner rounded-l p-2 flex-1' id='name'  placeholder='First Name, Last Name' />
       </form>
-        <h2 className = 'tracking-wide text-xl mt-10 ml-20'> In-Class Participation:</h2>
+      </div> 
+
+      <div>  
+       <h2 className = 'tracking-wide text-xl mt-10 ml-20'> Enter Student/Guardian Email:</h2>
+        <form className=' w-1/2 mt-2 flex '>
+        <input type="text" className=' w-12 bg-[#eff2f7] ml-20 shadow-inner rounded-l p-2 flex-1' id='name'  placeholder='First Name, Last Name' />
+      </form>
+      </div> 
+
+  <div>
+   <h2 className = 'tracking-wide text-xl mt-10 ml-20'> In-Class Participation:</h2>
    <HoverRating className = 'mt-5 ml-20' size = 'large'></HoverRating>
-   <h2 className = 'tracking-wide text-xl mt-10 ml-20'> Productivity:</h2>
+   </div>
+
+   <div>
+   <h2 className = 'tracking-wide text-xl mt-10 ml-20'> Productivity & Focus :</h2>
    <HoverRating className = 'mt-5 ml-20' size = 'large'></HoverRating>
+   </div>
+
+   <div>
    <h2 className = 'tracking-wide text-xl mt-10 ml-20'> Understanding of Material:</h2>
-   <HoverRating className = 'mt-5 ml-20' size = 'large'></HoverRating>
+   <HoverRating className = 'mt-5 ml-5' size = 'large'></HoverRating>
+   </div>
+
+
+   <div>
+   <h2 className = 'tracking-wide text-xl mt-10 ml-20'> Organization and Prepardness:</h2>
+   <HoverRating className = 'mt-5 ml-5' size = 'large'></HoverRating>
+   </div>
+
+   <div>
+   <h2 className = 'tracking-wide text-xl mt-10 ml-20'> Overall Attitude & Vibes: </h2>
+   <RadioGroupRating></RadioGroupRating>
+   </div>
+
+   <div> 
+    <button className = 'font-normal text-lg rounded-full text-white bg-[#735972] mt-16 ml-20'> &nbsp; Submit Feedback &nbsp;  </button>
+   </div>
+
+    </div>
     </div>
   )
 }
