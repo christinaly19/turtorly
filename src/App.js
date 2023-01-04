96974cd4e2311f92587ccc11821c568ebf35cb0c
Christina Li <christinayxli@gmail.com>
2023-01-04 16:32:55 -0500

    linked about page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css'
 import Peformance from './components/peformance'
 import Homepage from './components/homepage';
 import { BrowserRouter, Routes, Route, useNavigate} from 'react-router-dom';
+import About from './components/about';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
     <Routes>
     <Route path="/tutorly" element={<Homepage/>} />
     <Route path="/tutorly/peformance" element={<Peformance/>} />
+    <Route path ="/tutorly/about" element = {<About/>}></Route>
   </Routes>
   </div>
   );
