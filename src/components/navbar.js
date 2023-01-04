e27faa96dd7259fdd615c54e35288c0be82a6752
Christina Li <christinayxli@gmail.com>
2023-01-04 16:38:39 -0500

    fixed react routing

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -10,6 +10,9 @@ function Navbar({current, buttontxt, className, showbutton}) {
 
   const navigateToHome = () => {
     navigate('/tutorly');
+  }
+    const navigateToAbout = () => {
+      navigate('/tutorly/about');
   };
   return (
     <nav className = {className} >
@@ -35,7 +38,7 @@ function Navbar({current, buttontxt, className, showbutton}) {
           <a href="/" class="block py-2 pl-3 pr-4 font-bold md:bg-transparent hover:text-[#C3AAB0] md:text-black md:p-0 dark:text-white" aria-current="page">{current}</a>
         </li>
         <li>
-          <a href="/tutorly/about" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#C3AAB0] md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+          <a onClick = {navigateToAbout} class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#C3AAB0] md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
         </li>
         <li>
           <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#C3AAB0] md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
