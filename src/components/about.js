b05a13d07719410cee1e2fe577784bd2701f1058
Christina Li <christinayxli@gmail.com>
2023-01-05 13:25:52 -0500

    hover effects on about page

--- src/components/about.js
+++ src/components/about.js
@@ -4,7 +4,9 @@ import Navbar from './navbar'
 import circle1 from '../assets/circle1.png'
 import circle2 from '../assets/circle2.png'
 import circle3 from '../assets/circle3.png'
+import {useState} from 'react';
 function About() {
+ 
   return (
     <div>
         <Navbar buttontxt = "Performance"showbutton = {true} aboutbold = {true} current = 'Home' className = 'bg-[#f4e9e3] px-2 sm:px-4 lg:px-6 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0'></Navbar>
@@ -14,12 +16,15 @@ function About() {
         progress reports and updates between tutors and students/parents. With Tutor.ly's unique performance page, tutors can easily grade students on learning by 
         scoring students on key areas such as productivity, focus, understanding, homework completiton, and more. Tutors can then easily download the page, and send it to parent emails 
         with only a few mouse clicks — freeing up more time for learning and teaching. </p>
-        <div className = 'mt-16 ml-20 grid grid-cols-3'>
-            <div className = 'card hover:'>  <img alt = 'phone' src={circle1}/> 
+        <div className = 'mr-20 ml-20 mt-20 grid grid-cols-3'>
+            <div className = 'card hover:border-2 border-[#eae1ea]'>  <img alt = 'phone' src={circle1}/> 
+        
             </div>
-            <div className = 'card'>  <img alt = 'phone' src={circle2}/> </div>
-            <div className= 'card'>  <img alt = 'phone' src={circle3}/> </div>
-           
+      
+            <div  className = 'card hover:border-2 border-[#eae1ea]'>  <img alt = 'phone' src={circle2}/>
+       
+             </div>
+            <div className= 'card hover:border-2 border-[#eae1ea]'>  <img alt = 'phone' src={circle3}/> </div>
         </div>
     </div>
   )
